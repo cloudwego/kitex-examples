ca2ab1bca73f0c0e4ea1e33f45b0f1eebb06894d
wangzhuowei <wangzhuowei@bytedance.com>
2022-11-17 16:58:57 +0800

    feat: add profiler server example

---
+++ profiler/client/main.go
@@ -0,0 +1,42 @@
+// Copyright 2021 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/cloudwego/kitex/client"
+
+	"github.com/cloudwego/kitex-examples/kitex_gen/api"
+	"github.com/cloudwego/kitex-examples/kitex_gen/api/echo"
+)
+
+// make server busy
+func main() {
+	client, err := echo.NewClient("echo", client.WithHostPorts("0.0.0.0:8888"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		req := &api.Request{Message: "my request"}
+		resp, err := client.Echo(context.Background(), req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(resp)
+	}
+}
