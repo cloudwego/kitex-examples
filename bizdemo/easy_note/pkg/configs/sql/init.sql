56d020f3fe62d6fc915376dff16ce3d13224b332
Lorain <743804605@qq.com>
2022-11-28 14:40:17 +0800

    feat: use docker to init database

---
+++ bizdemo/easy_note/pkg/configs/sql/init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `user`
+(
+    `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `user_name`  varchar(128) NOT NULL DEFAULT '' COMMENT 'UserName',
+    `password`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Password',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'User account create time',
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'User account update time',
+    `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'User account delete time',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='User account table';
+
+CREATE TABLE `note`
+(
+    `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `user_id`    int(64) NOT NULL DEFAULT 0 COMMENT 'UserID',
+    `title`      varchar(128) NOT NULL DEFAULT '' COMMENT 'Title',
+    `content`    TEXT NULL COMMENT 'Content',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Note creates time',
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Note updates time',
+    `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'Note deletes time',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Note table';
