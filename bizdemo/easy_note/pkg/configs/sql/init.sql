41b7119e34edd66954632a456b3008bfc67051ea
Lorain <743804605@qq.com>
2022-11-30 00:05:47 +0800

    update comment

--- bizdemo/easy_note/pkg/configs/sql/init.sql
+++ bizdemo/easy_note/pkg/configs/sql/init.sql
@@ -16,8 +16,8 @@ CREATE TABLE `note`
     `user_id`    int(64) NOT NULL DEFAULT 0 COMMENT 'UserID',
     `title`      varchar(128) NOT NULL DEFAULT '' COMMENT 'Title',
     `content`    TEXT NULL COMMENT 'Content',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Note creates time',
-    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Note updates time',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Note create time',
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Note update time',
     `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'Note delete time',
     PRIMARY KEY (`id`),
     KEY          `idx_user_id_title` (`user_id`, `title`) COMMENT 'UserID Title index'
