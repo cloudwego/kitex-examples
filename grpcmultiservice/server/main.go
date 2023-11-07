6e06ee3b39d4520cbf4977bc20070040eb2e1465
Marina-Sakai <marina.sakai@bytedance.com>
2023-11-07 20:45:45 +0800

    fix: update kitex design

--- grpcmultiservice/server/main.go
+++ grpcmultiservice/server/main.go
@@ -19,25 +19,21 @@ import (
 	"log"
 	"net"
 
-	"github.com/cloudwego/kitex/pkg/serviceinfo"
 	"github.com/cloudwego/kitex/server"
 
-	"github.com/cloudwego/kitex-examples/grpcmultiservice/kitex_gen/multiservice"
 	"github.com/cloudwego/kitex-examples/grpcmultiservice/kitex_gen/multiservice/servicea"
 	"github.com/cloudwego/kitex-examples/grpcmultiservice/kitex_gen/multiservice/serviceb"
 	"github.com/cloudwego/kitex-examples/grpcmultiservice/kitex_gen/multiservice/servicex"
 )
 
 func main() {
-	var svcs []serviceinfo.Service
-	svca := servicea.BuildServiceAService(new(ServiceAImpl))
-	svcb := serviceb.BuildServiceBService(new(ServiceBImpl))
-	svcx := servicex.BuildServiceXService(new(ServiceXImpl))
-	svcs = append(svcs, svca, svcb, svcx)
-
 	addr, _ := net.ResolveTCPAddr("tcp", "localhost:8888")
-	svr := multiservice.NewServerWithMultiServices(svcs, server.WithServiceAddr(addr))
-	err := svr.Run()
+
+	svr := server.NewServer(server.WithServiceAddr(addr))
+	err := svr.RegisterService(servicea.NewServiceInfo(), new(ServiceAImpl))
+	err = svr.RegisterService(serviceb.NewServiceInfo(), new(ServiceBImpl))
+	err = svr.RegisterService(servicex.NewServiceInfo(), new(ServiceXImpl))
+	err = svr.Run()
 	if err != nil {
 		log.Println(err.Error())
 	}
