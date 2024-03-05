61fd8182b0183e638f16cd501dddd105a916dcaa
Marina-Sakai <marina.sakai@bytedance.com>
2024-03-05 15:27:54 +0800

    fix: update grpc usage

--- grpc_multi_service/server/main.go
+++ grpc_multi_service/server/main.go
@@ -20,6 +20,7 @@ import (
 	"net"
 
 	"github.com/cloudwego/kitex/server"
+
 	"grpc_multi_service/kitex_gen/multi/service/servicea"
 	"grpc_multi_service/kitex_gen/multi/service/serviceb"
 )
@@ -28,11 +29,11 @@ func main() {
 	addr, _ := net.ResolveTCPAddr("tcp", "localhost:8888")
 
 	svr := server.NewServer(server.WithServiceAddr(addr))
-	err := svr.RegisterService(servicea.NewServiceInfo(), new(ServiceAImpl))
+	err := servicea.RegisterService(svr, new(ServiceAImpl))
 	if err != nil {
 		log.Println(err.Error())
 	}
-	err = svr.RegisterService(serviceb.NewServiceInfo(), new(ServiceBImpl))
+	err = serviceb.RegisterService(svr, new(ServiceBImpl))
 	if err != nil {
 		log.Println(err.Error())
 	}
