2b3dd5e70285966bef3777383fefef9b5470f7c9
violapioggia <2604296771@qq.com>
2024-05-02 12:29:15 +0800

    test: check example_shop test

--- basic/example_shop/test.sh
+++ basic/example_shop/test.sh
@@ -7,7 +7,7 @@ REPO_PATH="."
 
 # 初始化状态变量
 status=0
-project="opentelemetry"
+project="example_shop"
 
 echo "---------------------------------------"
 echo "Running project: $project"
@@ -21,11 +21,22 @@ cd "$REPO_PATH" || exit
 docker-compose up -d
 cd - > /dev/null || exit
 
-# 启动 server
+# 启动 item server
 
-cd "$REPO_PATH/server" || exit
+cd "$REPO_PATH//rpc/item" || exit
 go run main.go > /dev/null 2>&1 &
-server_pid=$!
+item_server_pid=$!
+cd - > /dev/null || exit
+
+
+# 等待 server 启动
+sleep 1
+
+# 启动 stock server
+
+cd "$REPO_PATH/rpc/stock" || exit
+go run main.go > /dev/null 2>&1 &
+stock_server_pid=$!
 cd - > /dev/null || exit
 
 
@@ -34,7 +45,7 @@ sleep 1
 
 # 启动 client
 
-cd "$REPO_PATH/client" || exit
+cd "$REPO_PATH/api" || exit
 go run main.go > /dev/null 2>&1 &
 client_pid=$!
 cd - > /dev/null || exit
@@ -44,7 +55,7 @@ cd - > /dev/null || exit
 sleep 1
 
 # 检查 server 和 client 是否仍在运行
-if kill -0 $server_pid && kill -0 $client_pid; then
+if kill -0 item_server_pid && kill -0 stock_server_pid && kill -0 $client_pid; then
     echo "Project run successfully: $project"
     echo "---------------------------------------"
 else
@@ -54,7 +65,7 @@ else
 fi
 
 # 杀死 server 和 client
-kill $server_pid $client_pid
+kill $stock_server_pid $item_server_pid $client_pid
 
 
 # 设置脚本的退出状态
