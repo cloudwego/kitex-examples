a52bc530d9728d27e65fe71192a3b23a1a2de43f
ahao@butnoice.com <1993584108@qq.com>
2024-12-30 14:28:01 +0800

    chore: delete seata-go example

--- seata_go/kitex_gen/order/k-consts.go
+++
@@ -1,4 +0,0 @@
-package order
-
-// KitexUnusedProtection is used to prevent 'imported and not used' error.
-var KitexUnusedProtection = struct{}{}
--- seata_go/kitex_gen/storage/k-consts.go
+++
@@ -1,4 +0,0 @@
-package storage
-
-// KitexUnusedProtection is used to prevent 'imported and not used' error.
-var KitexUnusedProtection = struct{}{}
